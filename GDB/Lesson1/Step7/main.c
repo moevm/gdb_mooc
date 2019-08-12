0cc350947ac7c4d5ecd8d3ab7dc5e2cadf8c32d1
Ksenox <xxxtestxxx@yandex.ru>
2019-08-12 18:00:33 +0300

    Fixes for Lesson 1 Step 7

--- GDB/Lesson1/Step7/main.c
+++ GDB/Lesson1/Step7/main.c
@@ -1,53 +1,53 @@
 #include <stdbool.h>
 #include <stdio.h>
-
 void bar5(){
-	return;
+return ;
 }
-
+;
 void foo5(){
-	return;
+return ;
 }
-
-void bar4(){
-	foo5();
-	bar5();
+;
+void o_6479abc9f4455b2155dac6b9ea52327f(){
+foo5();
+bar5();
 }
-
-void foo4(){
-	bar4();
-	bar4();
-	foo5();
+;
+void o_da2dc608af8b20652eeaf349bbeb1f11(){
+o_6479abc9f4455b2155dac6b9ea52327f();
+o_6479abc9f4455b2155dac6b9ea52327f();
+foo5();
 }
-
-void foo3(){
-	bar4();
-	foo4();
+;
+void o_7cb76b889a3ad4bcc03a98714ac2471b(){
+o_6479abc9f4455b2155dac6b9ea52327f();
+o_da2dc608af8b20652eeaf349bbeb1f11();
 }
-
-void bar3(){
-	foo4();
+;
+void o_ad9324e1fb1e5c923ad74ec3fb29eabf(){
+o_da2dc608af8b20652eeaf349bbeb1f11();
 }
-
-void bar2(){
-	foo3();
+;
+void o_ffbb1536103b54ad5012a9cd9b16c334(){
+o_7cb76b889a3ad4bcc03a98714ac2471b();
 }
-
-void bar1(){
-	foo3();
-	bar3();
+;
+void o_4bca162ede54436fb91797230a85a3a8(){
+o_7cb76b889a3ad4bcc03a98714ac2471b();
+o_ad9324e1fb1e5c923ad74ec3fb29eabf();
 }
-
-void foo2(){
-	bar2();
+;
+void o_719d7c61287bf2a09df9d04699956c07(){
+o_ffbb1536103b54ad5012a9cd9b16c334();
 }
-
-void foo1(){
-	foo2();
-	bar1();
+;
+void o_4f8bccc384b8e2841362489e11e2035d(){
+o_719d7c61287bf2a09df9d04699956c07();
+o_4bca162ede54436fb91797230a85a3a8();
 }
-
-int main() {
-	foo1();
-  return 0;
+;
+int main(){
+o_4f8bccc384b8e2841362489e11e2035d();
+return (0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);
 }
+;

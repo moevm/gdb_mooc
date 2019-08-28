8c68ac4dfdcfa24c6908345b8e018b91930350bb
Ksenox <xxxtestxxx@yandex.ru>
2019-08-28 16:52:56 +0300

    Tasks 2-5 for Lesson 4

---
+++ GDB/Lesson4/Task4/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+char* str = "This is GDB Course";
+return 0;
+}
---
+++ GDB/Lesson4/Task5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+int a=4.6;
+bool b=true;
+float c=2.1*5-a/a+1-6.6;
+int d=a;
+bool e=false;
+bool f=true;
+bool g=false;
+int h=3%2/6.0;
+float i=7.6;
+int j=4%1+1%4-2%1+3*3+5%1/3*3;
+bool k=false;
+float l=j;
+return 0;
+}
